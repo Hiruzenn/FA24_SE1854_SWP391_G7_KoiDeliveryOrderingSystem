040e2e88092c82c0b903cfd3c03522311ee18c65
MTh1enn <thientnmse1735541@fpt.edu.vn>
2024-10-06 08:55:26 +0700

    Set up Security Config

---
+++ KoiDeliveryOrderingSystem-Backend/src/main/java/com/swp391/group7/KoiDeliveryOrderingSystem/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.swp391.group7.KoiDeliveryOrderingSystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@org.springframework.context.annotation.Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auth -> auth
+                        .anyRequest().permitAll()
+                ))
+                .build();
+    }
+}
---
+++ KoiDeliveryOrderingSystem-Backend/src/main/java/com/swp391/group7/KoiDeliveryOrderingSystem/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.swp391.group7.KoiDeliveryOrderingSystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
+}
