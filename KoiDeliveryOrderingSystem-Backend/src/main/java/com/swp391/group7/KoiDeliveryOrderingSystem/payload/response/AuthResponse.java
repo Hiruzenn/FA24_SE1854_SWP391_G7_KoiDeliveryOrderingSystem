83e13b8c57a80a6f609f7c71fde1650204ed0826
MTh1enn <thientnmse173541@fpt.edu.vn>
2024-10-13 13:18:56 +0700

    fix/User-Login api

--- KoiDeliveryOrderingSystem-Backend/src/main/java/com/swp391/group7/KoiDeliveryOrderingSystem/payload/response/AuthResponse.java
+++ KoiDeliveryOrderingSystem-Backend/src/main/java/com/swp391/group7/KoiDeliveryOrderingSystem/payload/response/AuthResponse.java
@@ -1,5 +1,6 @@
 package com.swp391.group7.KoiDeliveryOrderingSystem.payload.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -8,6 +9,8 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AuthResponse {
     String token;
+    String role;
 }
