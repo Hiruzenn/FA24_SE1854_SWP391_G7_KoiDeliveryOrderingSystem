f300822341d995e625b665e8b2b652ca393481b7
MTh1enn <thientnmse173541@fpt.edu.vn>
2024-11-01 10:34:57 +0700

    data seed user and healtServiceCategory

--- KoiDeliveryOrderingSystem-Backend/src/main/java/com/swp391/group7/KoiDeliveryOrderingSystem/config/DataLoader.java
+++ KoiDeliveryOrderingSystem-Backend/src/main/java/com/swp391/group7/KoiDeliveryOrderingSystem/config/DataLoader.java
@@ -1,13 +1,9 @@
 package com.swp391.group7.KoiDeliveryOrderingSystem.config;
 
-import com.swp391.group7.KoiDeliveryOrderingSystem.entity.DeliveryMethod;
+import com.swp391.group7.KoiDeliveryOrderingSystem.entity.*;
+import com.swp391.group7.KoiDeliveryOrderingSystem.entity.Enum.CustomerStatusEnum;
 import com.swp391.group7.KoiDeliveryOrderingSystem.entity.Enum.SystemStatusEnum;
-import com.swp391.group7.KoiDeliveryOrderingSystem.entity.FishCategory;
-import com.swp391.group7.KoiDeliveryOrderingSystem.entity.Role;
-import com.swp391.group7.KoiDeliveryOrderingSystem.repository.DeliveryMethodRepository;
-import com.swp391.group7.KoiDeliveryOrderingSystem.repository.FishCategoryRepository;
-import com.swp391.group7.KoiDeliveryOrderingSystem.repository.RoleRepository;
-import com.swp391.group7.KoiDeliveryOrderingSystem.repository.UserRepository;
+import com.swp391.group7.KoiDeliveryOrderingSystem.repository.*;
 import jakarta.transaction.Transactional;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -19,8 +15,13 @@ import org.springframework.stereotype.Component;
 @Transactional
 public class DataLoader {
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+    String encodedPassword = passwordEncoder.encode("123456");
+
     @Bean
-    CommandLineRunner initData(UserRepository userRepository, RoleRepository roleRepository, DeliveryMethodRepository deliveryMethodRepository, FishCategoryRepository fishCategoryRepository) {
+    CommandLineRunner initData(UserRepository userRepository, RoleRepository roleRepository,
+                               DeliveryMethodRepository deliveryMethodRepository, FishCategoryRepository fishCategoryRepository,
+                               HealthServiceCategoryRepository healthServiceCategoryRepository
+    ) {
         return args -> {
             if (roleRepository.count() == 0) {
                 Role adminRole = Role.builder()
@@ -39,10 +40,43 @@ public class DataLoader {
                 roleRepository.save(customersRole);
                 roleRepository.save(managerRole);
                 roleRepository.save(staffRole);
-
-
             }
-            if(deliveryMethodRepository.count() == 0){
+            if (userRepository.count() == 0) {
+
+                Users user = Users.builder()
+                        .name("User")
+                        .email("user@gmail.com")
+                        .password(encodedPassword)
+                        .role(roleRepository.findByName("CUSTOMERS"))
+                        .customerStatus(CustomerStatusEnum.VERIFIED)
+                        .build();
+                Users staff = Users.builder()
+                        .name("Staff")
+                        .email("staff@gmail.com")
+                        .password(encodedPassword)
+                        .role(roleRepository.findByName("STAFF"))
+                        .customerStatus(CustomerStatusEnum.VERIFIED)
+                        .build();
+                Users manager = Users.builder()
+                        .name("Manager")
+                        .email("manager@gmail.com")
+                        .password(encodedPassword)
+                        .role(roleRepository.findByName("MANAGER"))
+                        .customerStatus(CustomerStatusEnum.VERIFIED)
+                        .build();
+                Users admin = Users.builder()
+                        .name("Admin")
+                        .email("admin@gmail.com")
+                        .password(encodedPassword)
+                        .role(roleRepository.findByName("ADMIN"))
+                        .customerStatus(CustomerStatusEnum.VERIFIED)
+                        .build();
+                userRepository.save(user);
+                userRepository.save(staff);
+                userRepository.save(manager);
+                userRepository.save(admin);
+            }
+            if (deliveryMethodRepository.count() == 0) {
                 DeliveryMethod van = DeliveryMethod.builder()
                         .deliveryMethodName("VAN")
                         .status(SystemStatusEnum.AVAILABLE)
@@ -64,7 +98,7 @@ public class DataLoader {
                 deliveryMethodRepository.save(boat);
                 deliveryMethodRepository.save(train);
             }
-            if(fishCategoryRepository.count() == 0){
+            if (fishCategoryRepository.count() == 0) {
                 FishCategory fishA = FishCategory.builder()
                         .fishCategoryName("Type A")
                         .fishCategoryDescription("Super A")
@@ -83,6 +117,43 @@ public class DataLoader {
                 fishCategoryRepository.save(fishA);
                 fishCategoryRepository.save(fishB);
                 fishCategoryRepository.save(fishC);
+            }
+            if (healthServiceCategoryRepository.count() == 0) {
+                HealthServiceCategory healthServiceCategoryA = HealthServiceCategory.builder()
+                        .serviceName("Prepare oxygen bag and pump")
+                        .serviceDescription("Ensure adequate oxygen and space for the fish.")
+                        .price(Float.parseFloat(String.valueOf(500.0)))
+                        .status(SystemStatusEnum.AVAILABLE)
+                        .build();
+                HealthServiceCategory healthServiceCategoryB = HealthServiceCategory.builder()
+                        .serviceName("Add salt or anti-stress medication")
+                        .serviceDescription("Reduce stress and risk of infection.")
+                        .price(Float.parseFloat(String.valueOf(400.0)))
+                        .status(SystemStatusEnum.AVAILABLE)
+                        .build();
+                HealthServiceCategory healthServiceCategoryC = HealthServiceCategory.builder()
+                        .serviceName("Maintain proper temperature and lighting")
+                        .serviceDescription("Stabilize the transport environment")
+                        .price(Float.parseFloat(String.valueOf(500.0)))
+                        .status(SystemStatusEnum.AVAILABLE)
+                        .build();
+                HealthServiceCategory healthServiceCategoryD = HealthServiceCategory.builder()
+                        .serviceName("Use insulated boxes")
+                        .serviceDescription("Protect fish from thermal shock and vibration.")
+                        .price(Float.parseFloat(String.valueOf(500.0)))
+                        .status(SystemStatusEnum.AVAILABLE)
+                        .build();
+                HealthServiceCategory healthServiceCategoryE = HealthServiceCategory.builder()
+                        .serviceName("Tracking and post-shipment support")
+                        .serviceDescription("Update information and instruct recipients on how to care for fish.")
+                        .price(Float.parseFloat(String.valueOf(500.0)))
+                        .status(SystemStatusEnum.AVAILABLE)
+                        .build();
+                healthServiceCategoryRepository.save(healthServiceCategoryA);
+                healthServiceCategoryRepository.save(healthServiceCategoryB);
+                healthServiceCategoryRepository.save(healthServiceCategoryC);
+                healthServiceCategoryRepository.save(healthServiceCategoryD);
+                healthServiceCategoryRepository.save(healthServiceCategoryE);
             }
         };
     }
